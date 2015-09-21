189df6f5e019dc63516aed94b3f6ba848a636ebf
Ashar Fuadi <fushar@gmail.com>
2015-09-21 15:19:38 +0700

    Adapt to apis -> api change in jophiel-commons

--- app/org/iatoki/judgels/jophiel/controllers/api/AbstractJophielAPIController.java
+++ app/org/iatoki/judgels/jophiel/controllers/api/AbstractJophielAPIController.java
@@ -4,7 +4,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.iatoki.judgels.jophiel.AccessToken;
 import org.iatoki.judgels.jophiel.User;
-import org.iatoki.judgels.jophiel.apis.JophielUserAPIIdentity;
+import org.iatoki.judgels.jophiel.api.JophielUserAPIIdentity;
 import org.iatoki.judgels.jophiel.services.ClientService;
 import org.iatoki.judgels.jophiel.services.UserService;
 import org.iatoki.judgels.play.apis.JudgelsAPIUnauthorizedException;
